2b7b86fb141798f168bf5739ff5e95edc244c5ee
Md Jawed <mdjawedh22@gmail.com>
2023-08-29 05:07:25 +0530

    banner task complete

--- src/Pages/MainLanding.jsx
+++ src/Pages/MainLanding.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import "../styles/mainLanding.css"
+import { Link } from 'react-router-dom'
+import imgbanner from "../Images/banner-1.png"
 
 const MainLanding = () => {
   return (
@@ -8,12 +10,22 @@ const MainLanding = () => {
       <Navbar/>
       <div className="main-container">
         <div className="banner">
+          {/* -----------------------text-in-banner-section------------- */}
              <div className="text-banner-section">
-              <h1>INVESTMENT STACK</h1>
+            <div className='investment-stock'> <h1>INVESTMENT STOCK</h1>
               <h1>for India</h1>
 
-              <h3>Elevate Your Investment Strategy with Our <span>Comprehensive APIs</span> </h3>
+              <p>Elevate Your Investment Strategy with Our <span>Comprehensive APIs</span> </p></div>
+             <div className='inestment-info'>
+                <p>Launch financial services in a matter of days. Take your pick from our flexible APIs, SDKs, or ready-to-use screens.</p>
+                <Link>See our products</Link>
+                <button>Contact Us</button>
              </div>
+             </div>
+             {/* -------------banner-img------------- */}
+           <div className='img-banner'>
+           <img src={imgbanner} alt="" />
+           </div>
         </div>
       </div>
     </div>
